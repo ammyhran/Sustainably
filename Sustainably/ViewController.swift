30384e605b8ee9029bd09ba012b51566d809c3d2
Alice Myhran <ammyhran@gmail.com>
2020-07-14 13:43:32 -0700

    Initial Push

---
+++ Sustainably/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sustainably
+//
+//  Created by Apple on 7/14/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
