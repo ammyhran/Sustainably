f99bc1094d960635915ae3bf814055bc374a5988
Annie Reller <annie@rellers.com>
2020-07-16 13:48:36 -0700

    correct files from Annie finally

---
+++ GlobalStores.swift
@@ -0,0 +1,21 @@
+//
+//  GlobalStores.swift
+//  Sustainably
+//
+//  Created by Annie Reller on 7/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class GlobalStores: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Sustainably/GlobalPetition.swift
@@ -0,0 +1,21 @@
+//
+//  GlobalPetition.swift
+//  Sustainably
+//
+//  Created by Annie Reller on 7/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class GlobalPetition: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
