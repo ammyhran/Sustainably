e35a3f20727b9cc8fcd4ff790b7f84f1baf87ef0
Annie Reller <annie@rellers.com>
2020-07-16 10:35:37 -0700

    adding san fran

---
+++ cityOptionsPage.swift
@@ -0,0 +1,21 @@
+//
+//  cityOptionsPage.swift
+//  Sustainably
+//
+//  Created by Annie Reller on 7/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class cityOptionsPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
