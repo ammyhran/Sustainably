cf62a8cfb5400282ee588ad5a784fbac148a34dd
Annie Reller <annie@rellers.com>
2020-07-17 11:48:09 -0700

    ahshrewjhrevdkfg

--- Sustainably/SeattleClothingDonate.swift
+++ Sustainably/SeattleClothingDonate.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class SeattleClothingDonate: UIView {
+class SeattleClothingDonate: UIViewController {
     
-
+    override func viewDidLoad() {
+        
+    }
     
-    @IBAction func seattleButton(_ sender: Any) {
+    @IBAction func seattleButton1(_ sender: Any) {
         
         UIApplication.shared.open(URL(string: "https://seattlegoodwill.org")! as URL, options: [:], completionHandler: nil)
     }
     
     
-    @IBAction func dressButton(_ sender: Any) {
+    @IBAction func dressButton1(_ sender: Any) {
+ 
         UIApplication.shared.open(URL(string: "https://seattle.dressforsuccess.org")! as URL, options: [:], completionHandler: nil)
     }
     
     
-    
-    @IBAction func closetButton(_ sender: Any) {
+    @IBAction func button3(_ sender: Any) {
+  
         UIApplication.shared.open(URL(string: "https://locations.outofthecloset.org/wa-seattle-18112400")! as URL, options: [:], completionHandler: nil)
     }
     
