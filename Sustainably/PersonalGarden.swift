0d9baea6c864f88d8d1479cfab638abc69fc7bb2
Annie Reller <annie@rellers.com>
2020-07-16 18:12:04 -0700

    lots of new files

---
+++ Sustainably/PersonalGarden.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalGarden.swift
+//  Sustainably
+//
+//  Created by Annie Reller on 7/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class PersonalGarden: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Sustainably/SeattleGardens.swift
@@ -0,0 +1,21 @@
+//
+//  SeattleGardens.swift
+//  Sustainably
+//
+//  Created by Annie Reller on 7/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SeattleGardens: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
